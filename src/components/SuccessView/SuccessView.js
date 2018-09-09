0bf0da4363e1b52cb324bc5e7379ed54b648921e
Sam Bahta <smabahta37@gmail.com>
2018-09-09 15:22:29 -0500

    success view made

--- src/components/SuccessView/SuccessView.js
+++ src/components/SuccessView/SuccessView.js
@@ -1,5 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-class SuccessView extends Component{
+
+class SuccessView extends Component {
+
+    handleNewSurvey = (event) => {
+        this.props.dispatch({ type: 'CLEAR_FEEDBACK' });
+        this.props.history.push('/');
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Success!</h1>
+                <h3>
+                    Thank you for your feedback!
+            </h3>
+                <button onClick={this.handleNewSurvey}>Leave New Feedback</button>
+            </div>
+        )
+    }
 
 }//end class
+
+export default connect()(SuccessView);
