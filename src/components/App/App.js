6d7f257827420a33a314a8d13e0db1f5cec0c136
Sam Bahta <smabahta37@gmail.com>
2018-09-08 21:05:37 -0500

    views and routes made

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
-
+//import { HashRouter as Router, Route } from "react-router-dom";
+import Q1View from '../Q1View/Q1View'
+import Q2View from '../Q2View/Q2View'
+import Q3View from '../Q3View/Q3View'
+import SuccessView from '../SuccessView/SuccessView'
+import { HashRouter as Router, Route } from "react-router-dom";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
-        </header>
-        <br/>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Feedback!</h1>
+            <h4><i>Don't forget it!</i></h4>
+          </header>
+          <br />
+          {/* CLient-side Routers */}
+          <Route exact path="/" component={Q1View} />
+          <Route path="/2" component={Q2View} />
+          <Route path="/3" component={Q3View} />
+          <Route path="/4" component={SuccessView} />
+        </div>
+      </Router>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
+
