58edc38c56a6d52094f4c10c2a84a97003b7c372
Sam Bahta <smabahta37@gmail.com>
2018-09-10 10:01:40 -0500

    flag function fixed and working properly

--- server/server.js
+++ server/server.js
@@ -23,9 +23,10 @@ app.post('/feedback', (req, res) => {
         })
 })
 app.put('/feedback/:id', (req, res) => {
-    const flag = req.body;
+    console.log(req.params.id);
+    
     const query = `UPDATE "feedback" SET "flagged" = 'true' WHERE "id" =  $1;`;
-    pool.query(query[flag.id]).then((results) => {
+    pool.query(query,[req.params.id]).then((results) => {
         res.sendStatus(201);
     }).catch((error) => {
         console.log('error', error);
